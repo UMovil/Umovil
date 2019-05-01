fb35a0e32fce652c6e0fe144da53478eb9556554
UMovil <50064656+UMovil@users.noreply.github.com>
2019-05-01 17:19:37 -0500

    Version 1.003 01052019 17:17 pm se creó activity_is

---
+++ app/src/main/java/com/codigo55/umovil/activity_is.java
@@ -0,0 +1,13 @@
+package com.codigo55.umovil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_is extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_is);
+    }
+}
