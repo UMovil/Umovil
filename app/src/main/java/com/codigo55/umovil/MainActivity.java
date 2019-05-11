e44508462cd47a007da711c1712739810444774b
UMovil <50064656+UMovil@users.noreply.github.com>
2019-05-11 15:26:27 -0500

    Versión 1.010 11052019 15:25 pm, Se creó nuevo bloque en el home de noticiasw

--- app/src/main/java/com/codigo55/umovil/MainActivity.java
+++ app/src/main/java/com/codigo55/umovil/MainActivity.java
@@ -25,33 +25,4 @@ public class MainActivity extends AppCompatActivity {
         Intent next = new Intent(this,activity_is.class);
         startActivity(next);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.share) {
-            Toast.makeText(getApplicationContext(), "You Click Share",
-                    Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.about) {
-            Toast.makeText(getApplicationContext(), "You Click About",
-                    Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.exit) {
-            Toast.makeText(getApplicationContext(), "You Click Exit",
-                    Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.home) {
-            Toast.makeText(getApplicationContext(), "You Click Home",
-                    Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.search) {
-            Toast.makeText(getApplicationContext(), "You Click Search",
-                    Toast.LENGTH_SHORT).show();
-        }
-        return true;
-    }
 }
